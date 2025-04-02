d89a28b581b32047f862c48e2984685ff973f2e0
vegsk <vsk@insertlogic.io>
2025-04-02 14:25:11 -0400

    added first formula

---
+++ Formula/o8-cli.rb
@@ -0,0 +1,11 @@
+class Mytool < Formula
+  desc "O8-Cli"
+  homepage "https://github.com/Insert-Logic/homebrew-o8-cli"
+  url ""
+  sha256 ""
+  version "1.0.0"
+
+  def install
+    bin.install "cli"
+  end
+end
